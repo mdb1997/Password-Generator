a908e077cf65a6371547b552772cd4263330f423
mdb1997 <140769684+mdb1997@users.noreply.github.com>
2023-09-22 19:13:58 -0400

    Prompt for charLength w/ if statement & return

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-// Create variables for the password's end result
+// Create variables for the password's end result.
 var charLength = 8;
 var passResult = [];
-// Create variables for character criteria
+// Create variables for character criteria.
 var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 var numbersChar = ['1','2','3','4','5','6','7','8','9','0'];
@@ -17,7 +17,7 @@ generateBtn.addEventListener("click", writePassword);
 
 
 
-// Write password to the #password input
+// Write password to the #password input.
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
@@ -26,7 +26,19 @@ function writePassword() {
 
 }
 
-// Create a function for generatePassword
+// Create a function for generatePassword.
 function generatePassword() {
 
+}
+
+// Create a function specifically for the prompts.
+function getPrompts() {
+    // Write a prompt for the amount of characters.
+    // parseInt converts strings into numbers.
+    charLength = parseInt(prompt("Password length: Choose between 8 and 128 characters"));
+    // Write an if statement to insure for correct length. "NaN" = Not a Number.
+    if(isNaN(charLength) || charLength < 8 || charLength > 128) {
+        alert("Password does not meet criteria. Must be between 8 and 128 characters.");
+        return false;
+    }
 }
