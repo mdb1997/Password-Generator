82588af4b978b5fcec4eed4cbb14bfb0db787115
mdb1997 <140769684+mdb1997@users.noreply.github.com>
2023-09-22 18:53:32 -0400

    Created variables for the character criteria.

--- script.js
+++ script.js
@@ -2,6 +2,13 @@
 var charLength = 8;
 var passResult = [];
 
+// Create variables for character criteria
+var lowerCase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+var upperCase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+var numbersChar = ['1','2','3','4','5','6','7','8','9','0'];
+var specialChar = ['!','"','#','$','%','&','(',')','*','+',',','-','.','/',':',';','<','=','>','?','@','[',']','^','_','{','}','|','~'];
+
+
 
 
 // Assignment Code
